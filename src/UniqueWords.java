2615b4645aaf8b9657407ca07b2cdd89e31a9eae
musarrat.ryana22@my.stjohns.edu <musarrat.ryana22@my.stjohns.edu>
2024-12-15 04:16:05 -0500

    Completed countUnique methods for the UniqueWords project

--- src/UniqueWords.java
+++ src/UniqueWords.java
@@ -1,37 +1,35 @@
 import java.util.ArrayList;
-
+import java.util.HashSet;
 public class UniqueWords
 {
-   /**
-		counts the number of unique strings in a list
-		@param list ArrayList of strings to be examined
-		@return number of unique strings in the list
-   */
+   /*
+    						counts the number of unique strings in a list
+							@param list ArrayList of strings to be examined
+							@return number of unique strings in the list
+    */
    public static int countUnique(ArrayList<String> list)
    {
-	  int count = 0;
-	  
-      for (int i = 0; i < list.size(); i++)
-      {		 for (int j = 0; j < list.size(); j++)
-		 {
-			
-		 }
-      }
-	  return count;
+	 /*Removed for loop from method because it kept returning an incorrect number of 
+	  * unique words (3 to be specific). I tried Boolean, adding a second condition 
+	  * statement and counter. Implemented a Hashing instead because it counts
+	  * the frequency of a unique word and ignores duplicate words. 
+	  */
+	   
+	  HashSet<String> uniqueWords = new HashSet<>(list);
+      return uniqueWords.size();
    }
-
    public static void main(String[] args)
    {
-      ArrayList <String> words = new ArrayList<>();
+      ArrayList<String> words = new ArrayList<>();
       words.add("apple");
-	  words.add("orange");
-	  words.add("blackboard");
-	  words.add("apple");
-	  words.add("orange");
-	  words.add("sun");
-	  words.add("moon");
-   
-	  int unique = countUnique(words);
+      words.add("orange");
+      words.add("blackboard");
+      words.add("apple");
+      words.add("orange");
+      words.add("sun");
+      words.add("moon");
+
+      int unique = countUnique(words);
       System.out.println(words + " has " + unique + " unique words");
    }
 }
